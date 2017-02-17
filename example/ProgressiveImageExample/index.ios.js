0979e9ce19ae05b253dfb6167dbe613f706e7f59
johan.dutoit <johan.dutoit@thestreet.com>
2017-02-17 15:22:13 +0000

    Added example project

---
+++ example/ProgressiveImageExample/index.ios.js
@@ -0,0 +1,3 @@
+import { AppRegistry } from 'react-native';
+import ProgressiveImageExample from './example';
+AppRegistry.registerComponent('ProgressiveImageExample', () => ProgressiveImageExample);
